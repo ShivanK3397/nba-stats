edcb09fe43d9de11057b89e319a6233fa5aa07be
Shivan Kathir <shivan.kathir@gmail.com>
2025-01-01 02:24:20 -0500

    created PlayerRepository and completed all get requests

--- src/main/java/com/fantasystats/nba_stats/player/PlayerService.java
+++ src/main/java/com/fantasystats/nba_stats/player/PlayerService.java
@@ -1,5 +1,48 @@
 package com.fantasystats.nba_stats.player;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class PlayerService {
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository){
+        this.playerRepository=playerRepository;
+
+    }
+
+    public List<Player> getPlayers(){
+        return playerRepository.findAll();
+    }
+
+    public List<Player> getPlayersFromTeam(String teamName){
+        return playerRepository.findAll().stream()
+                .filter(player->teamName.equals(player.getTeam()))
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Player> getPlayersByName (String searchText){
+        return playerRepository.findAll().stream()
+                .filter(player-> player.getName().toLowerCase().contains(searchText.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByPos(String searchText){
+        return playerRepository.findAll().stream()
+                .filter(player -> player.getPos().toLowerCase().contains(searchText.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByTeamAndPosition(String team, String position){
+        return playerRepository.findAll().stream()
+                .filter(player -> team.equals(player.getTeam()) && position.equals(player.getPos()))
+                .collect(Collectors.toList());
+    }
+
+
 }
